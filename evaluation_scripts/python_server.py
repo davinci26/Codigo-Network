bb385bccfecc24086f29f897919662f472929886
davinci <sotirisnan@gmail.com>
2018-07-04 16:29:31 +0100

    Push to Cloud

--- evaluation_scripts/python_server.py
+++ evaluation_scripts/python_server.py
@@ -23,7 +23,7 @@ def run():
     print('starting server...')
     # Server settings
     # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('127.0.0.1', 8020)
+    server_address = ('35.195.51.1', 8020)
     httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
     print('running server...')
     httpd.serve_forever()
