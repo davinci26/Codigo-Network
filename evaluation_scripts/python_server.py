b1d84975e92bf2d1961abe317959106deb9996bb
davinci26 <sotirisnan@evaluation.c.iotssc-team-15.internal>
2018-07-05 11:51:32 +0000

    new server

--- evaluation_scripts/python_server.py
+++ evaluation_scripts/python_server.py
@@ -1,32 +1,22 @@
-#!/usr/bin/env python3
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import os 
-# HTTPRequestHandler class
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
- 
-  # GET
-  def do_GET(self):
-        # Send response status code
-        self.send_response(200)
-        cwd = os.getcwd()
-        print(cwd)
-        f = open(cwd + "/evaluation_scripts/text.txt", 'rb')
-        self.send_header('Content-type',    'text/html')
-        print("Sending")
-        # self.send_header('Content-type',    'text/html')
-        self.end_headers()
-        self.wfile.write(f.read())
-        f.close()
-        return
- 
-def run():
-    print('starting server...')
-    # Server settings
-    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('35.195.51.1', 8020)
-    httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
-    print('running server...')
-    httpd.serve_forever()
-    
- 
-run()
+from flask import *
+app = Flask(__name__)
+import os
+
+@app.route("/test/", methods=['POST'])
+def send_file():
+    print("Someone requested a file!")
+    cwd = os.getcwd()
+    print(cwd)
+    response = send_from_directory(directory = cwd + '/evaluation_scripts/', filename='large_text.txt')
+    response.headers['my-custom-header'] = 'my-custom-status-0'
+    return response
+
+@app.route("/")
+def hello():
+    print("I am alive!")
+    return "Hello World!"
+
+if __name__ == '__main__':
+    print("Server Started")
+    app.run(host = '0.0.0.0', port=8020 )
+
