980e8b0bc01cece9374ffc247cda588adf4ac6e8
davinci <sotirisnan@gmail.com>
2018-07-05 15:03:11 +0100

    Server-client simulation

--- evaluation_scripts/user.py
+++ evaluation_scripts/user.py
@@ -4,17 +4,20 @@ import threading
 import time
 import threading
 import argparse
+import datetime
+import numpy as np
 lock = threading.Lock()
+import json
+results = []
 
 # Define a function for the thread
 def make_request():
     start = time.time()
     r = requests.post("http://35.206.132.245:8020/test/")
     end = time.time()
-    lock.acquire() # thread blocks at this line until it can obtain lock
-    # in this section, only one thread can be present at a time.
-    with open("./evaluation_scripts/results.txt", 'a+') as dataf:
-        dataf.write("Request time: {} \n".format(end-start))
+    lock.acquire()
+    print("Made request")
+    results.append(end-start)
     lock.release()
 
 
@@ -24,19 +27,37 @@ def make_simulation(size):
     for i in range(0,size):
         thread_list.append(threading.Thread(target=make_request))
 
-    for thread in thread_list:
+    for i, thread in enumerate(thread_list):
         try:
             thread.start()
         except:
             print ("Error: unable to start thread - reached thread {}".format(i))
+            size = i
 
-    for ii in range(0,i):
-        thread_list[ii].join()
+    for k in range(0,size):
+        thread_list[k].join()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Command Line Interface')
     parser.add_argument('--Threads', type=int, nargs='?',
-                        help='Number of users to spawn')
-    args = parser.parse_args()
-    make_simulation(args.Threads)
+                        help='Max number of users to spawn')
+    parser.add_argument('--Increment', type=int, nargs='?',
+                        help='User increment')
 
+    args = parser.parse_args()
+
+    for thread_no in range(1,args.Threads+1, args.Increment):
+        make_simulation(thread_no)
+        now = datetime.datetime.now()
+        print(results)
+        result_json = {'Date-time': now.strftime("%Y-%m-%d %H%M"),
+                       'Users': thread_no,
+                       'Avg Time': np.mean(results),
+                       'Std Time': np.std(results),
+                       'Results': results }
+
+        with open("./evaluation_scripts/results.txt", 'a+') as dataf:
+            json.dump(result_json, dataf)
+            dataf.write('\n')
+            
+        results = []
